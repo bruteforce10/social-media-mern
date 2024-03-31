183795dec033e853513d83c64bce24204845c5a0
bruteforce10 <audifirdi@gmail.com>
2024-03-31 12:50:00 +0700

    fix deploy

--- server/index.js
+++ server/index.js
@@ -29,7 +29,12 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.use(cors(corsConfig));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // FILE STORAGE
