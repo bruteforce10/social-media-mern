b080c68363303a12f0ef17af231fa1b7a53e2756
bruteforce10 <audifirdi@gmail.com>
2024-03-31 13:17:21 +0700

    add gitignoer

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -8,8 +8,8 @@ export const register = async (req, res) => {
     const { firstName, lastName, email, password, picturePath, friends } =
       req?.body;
 
-    const salt = await bcrypt?.genSalt();
-    const passwordHash = await bcrypt?.hash(password, salt);
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
 
     const newUser = new User({
       firstName,
@@ -36,7 +36,7 @@ export const login = async (req, res) => {
     const user = await User.findOne({ email: email });
     if (!user) return res.status(400).json({ msg: "User does not exist." });
 
-    const isMatch = await bcrypt?.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
